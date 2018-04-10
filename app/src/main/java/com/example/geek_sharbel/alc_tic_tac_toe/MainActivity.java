22a989eed7f71a8e76425c8ad0742875ae648855
sharbel93 <sharbel.chris@gmail.com>
2018-04-10 14:11:28 +0300

    adding fields in main activity class and xml

--- app/src/main/java/com/example/geek_sharbel/alc_tic_tac_toe/MainActivity.java
+++ app/src/main/java/com/example/geek_sharbel/alc_tic_tac_toe/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.geek_sharbel.alc_tic_tac_toe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void game3(View view) {
+        Intent intent = new Intent(this, ThreeByThreeActivity.class);
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }
+    }
+
+    public void game4(View view) {
+        Intent intent = new Intent(this, FourByFourActivity.class);
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }
+    }
+
+    public void game5(View view) {
+        Intent intent = new Intent(this, FiveByFiveActivity.class);
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }
     }
 }
