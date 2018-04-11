2d195189fea680cbc97b7f0eeacff4bad64c11d8
sharbel93 <sharbel.chris@gmail.com>
2018-04-11 13:06:12 +0300

    adding functions in adding four, five game mode

--- app/src/main/java/com/example/geek_sharbel/alc_tic_tac_toe/ThreeByThreeActivity.java
+++ app/src/main/java/com/example/geek_sharbel/alc_tic_tac_toe/ThreeByThreeActivity.java
@@ -192,19 +192,6 @@ public class ThreeByThreeActivity extends AppCompatActivity {
         updateScores();
         resetGame();
     }
-    // Todo: set icon
-//
-//    AlertDialog alertDialog = new AlertDialog.Builder(AlertDialogActivity.this).create();
-//alertDialog.setTitle("Alert Dialog");
-//alertDialog.setMessage("Welcome to dear user.");
-//alertDialog.setIcon(R.drawable.welcome);
-//
-//alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
-//        public void onClick(DialogInterface dialog, int which) {
-//            Toast.makeText(getApplicationContext(), "You clicked on OK", Toast.LENGTH_SHORT).show();
-//        }
-//    });
 
-//alertDialog.show();
 
 }
